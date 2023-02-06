2b9d9296a4b0328354a7c7df76d9d08733b983c3
vk1391 <87279407+vk1391@users.noreply.github.com>
2023-02-06 12:32:24 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-output "access_key" {
-  value = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-  sensitive = true
-}
-output "secret_key" {
-  value = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  sensitive = true
-}
+#output "access_key" {
+#  value = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+#  sensitive = true
+#}
+#output "secret_key" {
+#  value = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+#  sensitive = true
+#}
 output "internal_ip_address_node01_yandex_cloud" {
   value = "${yandex_compute_instance.node1.network_interface.0.nat_ip_address}"
 }
